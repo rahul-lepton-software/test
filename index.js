d0df3dae30d2d88540aeb9acea34cf2cbac92598
rahul-lepton-software <rahul.kalra@leptonsoftware.com>
2023-08-27 22:26:39 +0530

    main changes add

--- index.js
+++ index.js
@@ -4,8 +4,5 @@ function sum(a,b){
 
 console.log("rahul kalra")
 console.log("rahul")
-<<<<<<< HEAD
 console.log("main-dev")
-=======
 console.log("Main Changes")
->>>>>>> ed4889ba62be49999608dc76c8014838d93756e0
